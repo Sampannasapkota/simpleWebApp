0a9f254e52905cf02058072c47bcf39e60517d66
Sampannasapkota <henryatokpas12@gmail.com>
2025-09-04 00:53:54 +0545

    Initial commit

---
+++ src/main/java/com/springBoot/simpleWebApp/HomeController.java
@@ -0,0 +1,17 @@
+package com.springBoot.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+@RequestMapping("/")
+    public String car(){
+        return "I am Mercedes Benz.";
+    }
+    @RequestMapping("/about")
+    public String aboutUs(){
+        return "I am a classic and premium car";
+    }
+}
---
+++ src/main/java/com/springBoot/simpleWebApp/LoginController.java
@@ -0,0 +1,12 @@
+package com.springBoot.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @RequestMapping ("/login")
+    public String login(){
+        return "I am login page";
+    }
+}
